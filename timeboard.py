101853415b3b98ec67638b068bc67311f652de12
Yi Ou <yi.ou@adtech.com>
2016-01-31 18:06:18 +0100

    Improve arg parsing; add support for screenboard import

--- timeboard.py
+++ timeboard.py
@@ -12,10 +12,14 @@ def loadjson(f):
 def authenticate(cred_json):
     initialize(**cred_json)
 
-def import_json(timeboard_json):
-    dash_json = timeboard_json['dash']
-    title, description, graphs = dash_json['title'], dash_json['description'], dash_json['graphs']
-    api.Timeboard.create(title=title, description=description, graphs=graphs)
+def import_json(dash_json, t):
+    if t == "t":
+        timeboard_json = dash_json['dash']
+        title, description, graphs = timeboard_json['title'], timeboard_json['description'], timeboard_json['graphs']
+        api.Timeboard.create(title=title, description=description, graphs=graphs)
+    else:
+        board_title, description, widgets = dash_json['board_title'], dash_json['description'], dash_json['widgets']
+        api.Screenboard.create(board_title=board_title, description=description, widgets=widgets)
 
 def export_json(dash_id, f, t):
     dash_json = api.Timeboard.get(dash_id) if t == "t" else api.Screenboard.get(dash_id)
@@ -31,16 +35,21 @@ def main():
     """
     )
     parser.add_argument("-c", "--credentials", required=True, help="the json file containing api_key and app_key as dictionary entries")
-    parser.add_argument("-i", "--import_file", help="the json file to import dashboard definition from")
-    parser.add_argument("-e", "--export_file", help="the json file to export dashboard definition to")
+    mutex_group = parser.add_mutually_exclusive_group(required=True)
+    mutex_group.add_argument("-i", "--import_file", help="the json file to import dashboard definition from")
+    mutex_group.add_argument("-e", "--export_file", help="the json file to export dashboard definition to")
     parser.add_argument("-d", "--dash_id", help="the id of the dashboard to be exported")
-    parser.add_argument("-t", "--dash_type", help="the type (t or s) of the dashboard to be exported")
+    parser.add_argument("-t", "--dash_type", choices=['t', 's'], default='t', help="the type of the dashboard to be imported or exported")
     args = parser.parse_args()
+
+    if args.export_file and not args.dash_id:
+       parser.print_help()
+       sys.exit(1)
 
     authenticate(loadjson(args.credentials))
 
     if args.import_file:
-        import_json(loadjson(args.import_file))
+        import_json(loadjson(args.import_file), args.dash_type)
     elif args.export_file and args.dash_id:
         export_json(args.dash_id, args.export_file, args.dash_type)
 
